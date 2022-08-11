b083ade6f9bd4487a3196f77c2ed0cb39d7bbded
Mridu Dixit <mridu.dixit@virtueanalytics.com>
2022-08-11 22:35:51 +0530

    update

--- lib/ui/widget/deal_product-card.dart
+++ lib/ui/widget/deal_product-card.dart
@@ -19,10 +19,10 @@ class _DealProductCardState extends State<DealProductCard> {
     return Column(
       children: [
         Container(
-          padding: const EdgeInsets.fromLTRB(15,15,15,0),
+          padding: const EdgeInsets.all(0),
           decoration: BoxDecoration(
             color: const Color(0xFFE5E6E8),
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(10),
           ),
           child: Image.network(
               '${ClientService.cdnUrl}${product.product!.images![0]}',
@@ -76,17 +76,12 @@ class _DealProductCardState extends State<DealProductCard> {
   }
 
   Widget _gridItemFooter(DealProduct product, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 15,right: 15),
-      child: Container(
-        padding: const EdgeInsets.only(left: 5, right: 5),
-        height: 50,
+    return 
+       Container(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        height: 55,
         decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(15),
-            bottomRight: Radius.circular(15),
-          ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -103,6 +98,7 @@ class _DealProductCardState extends State<DealProductCard> {
             ),
             const SizedBox(height: 5),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   product.dealPrice!.offerPrice != null
@@ -121,11 +117,13 @@ class _DealProductCardState extends State<DealProductCard> {
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                )
+                ),
+                Icon(Icons.add)
               ],
-            )
+            ),
+            
           ],
-        ),
+        
       ),
     );
   }
@@ -135,13 +133,13 @@ class _DealProductCardState extends State<DealProductCard> {
     return Obx(
       () {
         return Padding(
-          padding: const EdgeInsets.only(top: 20),
+          padding: const EdgeInsets.only(top: 10),
           child: GridView.builder(
             itemCount: myController.dealProd.length,
             shrinkWrap: true,
             scrollDirection: Axis.vertical,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                childAspectRatio: 10 / 12,
+                childAspectRatio: 10 / 13,
                 crossAxisCount: 2,
                 mainAxisSpacing: 10,
                 crossAxisSpacing: 10),
@@ -151,12 +149,12 @@ class _DealProductCardState extends State<DealProductCard> {
                 // height: 100,
                 // width: 100,
                 child: OpenContainerWrapper(
+                    product: myController.dealProd[index].product,
                     child: GridTile(
                       header: _gridItemHeader(dProduct, index),
                       // footer: _gridItemFooter(dProduct, context),
                       child: _gridItemBody(dProduct),
-                    ),
-                    product: myController.dealProd[index].product),
+                    )),
               );
             },
           ),
