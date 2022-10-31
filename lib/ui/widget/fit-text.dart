006f4c54c732cdcfcc5092796366e9f5774bc368
Hitesh Kumar <hitesh@virtueanalytics.com>
2022-10-31 14:10:17 +0530

    fittext added

---
+++ lib/ui/widget/fit-text.dart
@@ -0,0 +1,25 @@
+import 'package:amber_bird/utils/ui-style.dart';
+import 'package:flutter/material.dart';
+
+class FitText extends StatelessWidget {
+  final String text;
+  TextStyle style = TextStyles.bodyFont;
+  FitText(this.text, {TextStyle? style, Key? key}) : super(key: key) {
+    if (style != null) {
+      this.style = style;
+    } else {
+      this.style = TextStyles.bodyFont;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Text(
+        text,
+        style: style,
+      ),
+    );
+  }
+}
