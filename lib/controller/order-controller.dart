7c0ebe2b9c6f5a173ea4f19878597356965e19ad
Hitesh Kumar <hitesh@virtueanalytics.com>
2022-11-06 13:35:19 +0530

    working on it

--- lib/controller/order-controller.dart
+++ lib/controller/order-controller.dart
@@ -15,15 +15,8 @@ class OrderController extends GetxController {
     getOrder(tag);
   }
 
-<<<<<<< HEAD
-   getOrder(String id) async {
-     var response =
-        await ClientService.get(path: 'order', id: id);
-=======
   getOrder(String id) async {
-    print(id);
     var response = await ClientService.get(path: 'order', id: id);
->>>>>>> 9ad39be4ef938592333e20eea16a1347a9937dfb
     if (response.statusCode == 200) {
       log(response.data.toString());
       orderDetail.value = Order.fromMap(response.data as Map<String, dynamic>);
