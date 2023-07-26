487a841e24398a36442c12d819a88fd10be5135d
himanshu17113 <98476250+himanshu17113@users.noreply.github.com>
2023-07-26 08:45:01 +0530

    button alignment and animation to fit in screen

--- lib/ui/widget/app-update.dart
+++ lib/ui/widget/app-update.dart
@@ -10,9 +10,7 @@ class AppUpdate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.hardEdge,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
       color: Colors.white,
       child: Stack(
         children: [
@@ -21,9 +19,7 @@ class AppUpdate extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Lottie.asset('assets/app-update.json',
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.fill,
-                  height: 250),
+                  width: MediaQuery.of(context).size.width, fit: BoxFit.fill, height: MediaQuery.of(context).size.height * .3),
               SizedBox(
                 height: 50,
               ),
@@ -34,14 +30,17 @@ class AppUpdate extends StatelessWidget {
                   style: TextStyles.bodyFontBold,
                 ),
               ),
-              MaterialButton(
-                onPressed: () {
-                  inAppReview.openStoreListing();
-                },
-                color: AppColors.primeColor,
-                child: Text(
-                  'Install update',
-                  style: TextStyles.bodyFontBold.copyWith(color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom: 5),
+                child: MaterialButton(
+                  onPressed: () {
+                    inAppReview.openStoreListing();
+                  },
+                  color: AppColors.primeColor,
+                  child: Text(
+                    'Install update',
+                    style: TextStyles.bodyFontBold.copyWith(color: Colors.white),
+                  ),
                 ),
               )
             ],
