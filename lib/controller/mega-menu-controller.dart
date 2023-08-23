7372607b696b65546d7e806d42c3e1cefe5abcf3
Mridu Dixit <mridudixit15@gmail.com>
2023-08-23 20:34:23 +0530

    SB-176

--- lib/controller/mega-menu-controller.dart
+++ lib/controller/mega-menu-controller.dart
@@ -150,8 +150,8 @@ class MegaMenuController extends GetxController {
               type: 'MULTI',
               text: detail['name']));
         });
-        subMenuList.add(
-            GenericTab(image: '', id: 'THEMES', type: 'MULTI', text: 'Theme'));
+        subMenuList.add(GenericTab(
+            image: '', id: 'THEMES', type: 'MULTI', text: 'We-Care'));
       }
     } else if (parentTab.type == 'CAT') {
       var payload = {"parentCategoryId": parentTab.id};
