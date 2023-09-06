551b68ce9c516f58678b14e83e8d631cb6981f8c
Shahin <shahinsiddiqui17237@gmail.com>
2023-09-06 18:57:33 +0530

    notification_ui

--- lib/ui/element/snackbar.dart
+++ lib/ui/element/snackbar.dart
@@ -4,70 +4,77 @@ import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/safe_area_values.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
+enum SnackBarType { info, success, warning, error }
 class snackBarClass {
-  static showToast(context, msg) {
-    // ScaffoldMessenger.of(context).showSnackBar(
-    //   SnackBar(
-    //     // action: SnackBarAction(
-    //     //   label: 'Action',
-    //     //   onPressed: () {
-    //     //     // Code to execute.
-    //     //   },
-    //     // ),
-    //     content: Padding(
-    //       padding: const EdgeInsets.all(10),
-    //       child: Text(
-    //         msg ?? "Please try again!",
-    //         style: TextStyles.titleFont
-    //             .copyWith(color: AppColors.white, fontWeight: FontWeight.bold),
-    //       ),
-    //     ),
-    //     // margin: EdgeInsets.only(
-    //     //     bottom: MediaQuery.of(context).size.height - 150,
-    //     //     right: 20,
-    //     //     left: 20),
-    //     duration: const Duration(milliseconds: 3000),
-    //     padding: const EdgeInsets.all(8),
-    //     behavior: SnackBarBehavior.floating,
-    //     backgroundColor: AppColors.primeColor,
-    //     shape: RoundedRectangleBorder(
-    //         borderRadius: const BorderRadius.only(
-    //             topLeft: Radius.circular(12), topRight: Radius.circular(12)),
-    //         side: BorderSide(
-    //           color: AppColors.white,
-    //           width: 1,
-    //         )),
-    //   ),
-    // );\
-    showTopSnackBar(
-      Overlay.of(context),
-      Card(
-            color: Colors.black26,
-            shape: RoundedRectangleBorder(
-                borderRadius:  BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+  static showToast(context, msg, {SnackBarType type = SnackBarType.info}) {
+    {
+      IconData iconData;
+      Color iconColor;
+      Color snackBarColor;
 
-            ),
-        child: Container(
-          padding: EdgeInsets.zero,
-          height: 100,
-          child: CustomSnackBar.info(
-            backgroundColor:Colors.black26,
-            iconRotationAngle: 10,
-            iconPositionTop: 10,
-            iconPositionLeft: 20,
-            icon: Icon(Icons.check_circle_rounded,color: Colors.green,size: 34,),
-            messagePadding: EdgeInsets.only(top: 22),
-            message:msg ?? "Please try again!",
-                    textStyle: TextStyles.headingFont2
-                        .copyWith(color: AppColors.white),
+      // Set the icon and colors based on the snackbar type
+      switch (type) {
+        case SnackBarType.success:
+          iconData = Icons.check_circle;
+          iconColor = Colors.green;
+          snackBarColor = Colors.green;
+          break;
+        case SnackBarType.warning:
+          iconData = Icons.warning;
+          iconColor = Colors.orange;
+          snackBarColor = Colors.orange;
+          break;
+        case SnackBarType.error:
+          iconData = Icons.error;
+          iconColor = Colors.red;
+          snackBarColor = Colors.red;
+          break;
+        default:
+          iconData = Icons.info;
+          iconColor = Color(0xffF7CE4D); // Default icon color
+          snackBarColor = Colors.black26; // Default snackbar background color
+          break;
+      }
+      showTopSnackBar(
+          Overlay.of(context),
+          Container(
+            width: double.infinity,
+            child: Card(
+              // color: Colors.black26,
+              color: snackBarColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15)),
+
+              ),
+              child: Container(
+                padding: EdgeInsets.zero,
+                height: 100,
+                child: CustomSnackBar.info(
+                  backgroundColor: Colors.black26,
+                  iconRotationAngle: 1,
+                  iconPositionTop: 10,
+                  iconPositionLeft: 20,
+                  textScaleFactor: 1.2,
+                  maxLines: 2,
+                  icon: Icon(
+                    iconData,
+                    color: iconColor,
+                    size: 50,
                   ),
-        ),
-        ),
-      snackBarPosition: SnackBarPosition.top,
-      animationDuration: const Duration(milliseconds: 3000),
-      safeAreaValues: SafeAreaValues(
-       top :false,
-      )
-    );
+                  messagePadding: EdgeInsets.only(top: 22,left :22),
+                  message: msg ?? "Please try again!",
+                  textStyle: TextStyles.bodyFont
+                      .copyWith(color: AppColors.white),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ),
+          ),
+          snackBarPosition: SnackBarPosition.top,
+          animationDuration: const Duration(milliseconds: 3000),
+          safeAreaValues: SafeAreaValues()
+      );
+    }
   }
 }
